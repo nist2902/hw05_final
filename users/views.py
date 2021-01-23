c855cda55dc9b40b77596c4ed0f9b822c6f47993
Nikolai <nist2902@yandex.ru>
2021-01-23 03:40:17 +0300

    правки после ревью

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
-#from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+#from django.core.mail import send_mail
 
 from .forms import CreationForm
 
