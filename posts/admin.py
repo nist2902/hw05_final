76c183e30626206ef92c17af85f1fbcb2a23ba33
Nikolai <nist2902@yandex.ru>
2021-01-23 09:23:44 +0300

    правки после ревью

--- posts/admin.py
+++ posts/admin.py
@@ -10,7 +10,13 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 
+class GroupAdmin(admin.ModelAdmin): 
+    list_display = ('pk', 'title', 'description', 'slug') 
+    search_fields = ('title', 'description',) 
+    empty_value_display = '-пусто-' 
+
+
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Comment)
 admin.site.register(Follow)
