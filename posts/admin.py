3504417427c09e2a85159887bcdcbd01237aab26
Nikolai <nist2902@yandex.ru>
2021-01-23 09:28:16 +0300

    правки после ревью

--- posts/admin.py
+++ posts/admin.py
@@ -4,16 +4,16 @@ from .models import Comment, Follow, Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("text", "pub_date", "author")
+    list_display = ("pk", "text", "pub_date", "author", "group")
     search_fields = ("text",)
     list_filter = ("pub_date",)
     empty_value_display = "-пусто-"
 
 
 class GroupAdmin(admin.ModelAdmin): 
-    list_display = ('pk', 'title', 'description', 'slug') 
-    search_fields = ('title', 'description',) 
-    empty_value_display = '-пусто-' 
+    list_display = ("pk", "title", "description", "slug") 
+    search_fields = ("title", "description",) 
+    empty_value_display = "-пусто-" 
 
 
 admin.site.register(Post, PostAdmin)
